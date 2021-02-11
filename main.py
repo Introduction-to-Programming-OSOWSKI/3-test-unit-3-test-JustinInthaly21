1623eb9fcb4b8babc98d766c7ae1b9910ffa8836
JustinInthaly21 <825134@mystma.org>
2021-02-11 19:10:05 +0000

    test3 1

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-#WRITE YOUR CODE IN THIS FILE
+def countB(w):
+    r = 0
+    for i in w:
+        if i == 'b':
+            r = r + 1
+    return r
+
+def removeLast(w, x):
+    e = 0
+    for i in w:
+        e = e + 1
+    return e - x
+    
+
+
+
+def sumBetweenOdd(x, y):
+    
+    q = 0
+    for i in range(1, y + 1):
+        if(i % 2 != 0):
+            print("{0}".format(i))
+            q = q + i
+    return q
+
+def firstLast(w):
+    if(w[0] == w[-1]):
+        return True
+    else:
+        return False
+    
